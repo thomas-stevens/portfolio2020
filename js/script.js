6d0a7894e21ec762b808331d26b42ca922e0cc2e
ThomasStevens <58775290+thomas-stevens@users.noreply.github.com>
2020-04-17 19:55:36 +0200

    +

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
   document.documentElement.setAttribute('data-theme', currentTheme);
 
-  if (currentTheme === 'dark') {
+  if (currentTheme === 'color') {
     toggleSwitch.checked = true;
   }
 }
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'color');
+    localStorage.setItem('theme', 'color');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
